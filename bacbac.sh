442b63f66fb86c715c3a95da83f0b94e1387d0f6
Julien <julien@esprit-libre.info>
2011-07-12 14:31:52 +0200

    Boucle pour la gestion de la frequence

--- bacbac.sh
+++ bacbac.sh
@@ -4,12 +4,17 @@
 ## Variables
 CP=/bin/cp
 RSYNC=/usr/bin/rsync
+CUT=/bin/cut
 CONF=bacbac.conf
 
+## Fonctions
+# Decoupage des lignes de la conf
+decoup () {
+	XDECOUPE=$(echo $1 | $CUT -d: -f$2)
+}
+
+
 ## Script
-
-
-
 echo "--- Lancement Backup ---"
 # Traitement du fichier de configuration ligne par ligne
 while read ligne
@@ -19,7 +24,22 @@ while read ligne
             "#" )  # Commentaires 
                 ;;      
             *   )   
-                echo $ligne
+				# ON traite la frequence
+                decoup $ligne 1
+				if [ $XDECOUPE = "h" ];then
+					# Synchro toutes les heures
+						echo heure
+				elif [ $XDECOUPE = "j" ]; then
+					# Synchro tous les jours 
+						echo jour
+				elif [ $XDECOUPE = "m" ]; then
+					# Synchro tous les mois
+						echo mois
+				else
+					# Erreur dans la variable de frequence
+					echo "La valeur de la frequence est mauvaise pour la ligne : $ligne"
+					exit 1 
+				fi
                 ;;      
         esac    
     done < $CONF 
