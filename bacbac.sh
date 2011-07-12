73be642af97c331ce2cc695b43ca532d96bc9564
Julien <julien@esprit-libre.info>
2011-07-12 13:52:34 +0200

    Fichier de conf + mise en place de la boucle sur les lignes

--- bacbac.sh
+++ bacbac.sh
@@ -2,10 +2,25 @@
 # v0.1 - Script de sauvegarde incremental
 
 ## Variables
-$CP=/bin/cp
-$RSYNC=/usr/bin/rsync
-$CONF=/home/kaulian/etc/bacbac.conf
+CP=/bin/cp
+RSYNC=/usr/bin/rsync
+CONF=bacbac.conf
+
+## Script
 
 
 
-
+echo "--- Lancement Backup ---"
+# Traitement du fichier de configuration ligne par ligne
+while read ligne
+	do
+        chr=${ligne:0:1}
+        case $chr in 
+            "#" )  # Commentaires 
+                ;;      
+            *   )   
+                echo $ligne
+                ;;      
+        esac    
+    done < $CONF 
+ 
